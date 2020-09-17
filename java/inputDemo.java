f7489934a57854a8e32f094700b27f61efb1fa18
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-09-17 10:25:56 -0500

    fixed some errors and added comments for each line of code

--- java/inputDemo.java
+++ java/inputDemo.java
@@ -3,19 +3,19 @@ inport java.util.Scanner;
 
 public class InputDemo {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); //creates the Scanner object and names it input
 
-        System.out.print("Enter an integer: ");
-        int myInt = input.nextInt();
-        System.out.print("You entered %d.%n%n", myInt);
+        System.out.print("Enter an integer: "); //prompts the user for an integer
+        int myInt = input.nextInt(); //method to read the integer
+        System.out.printf("You entered %d.%n%n", myInt); //displays the users input
 
-        System.out.print("Enter a double: ");
-        double myDouble = input.nextDouble();
-        System.out.printf("You entered %.2f.%n%n", myDouble);
+        System.out.print("Enter a double: "); //prompts the user for a double
+        double myDouble = input.nextDouble(); //method to read the double
+        System.out.printf("You entered %.2f.%n%n", myDouble); //displays the users input
 
-        System.out.print("Enter a string: ");
-        input.nextLine();
-        String myString = input.nextLine();
-        System.out.printf("You entered \"%s\".%n%n", myString);
+        System.out.print("Enter a string: "); //prompts the user for a string
+        input.nextLine(); //added because of how the .nextDouble method works. It reads only a double however when the user inputs a number they will press the Enter Key. The Enter Key is essentially a newLine character("\n") and is ignored by the nextDouble() method as it is not a double
+        String myString = input.nextLine(); //method to read string
+        System.out.printf("You entered \"%s\".%n%n", myString); //displays the users input
     }
 }
