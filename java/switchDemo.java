912a65cc30caaedb32898cef0a1b25d75788c4c8
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-09-24 19:13:19 -0500

    adding comments to the switch demo

--- java/switchDemo.java
+++ java/switchDemo.java
@@ -7,22 +7,22 @@ public class SwitchDemo{
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter your grade: ");
-        String userGrade = input.nextLine().toUpperCase();
+        System.out.print("Enter your grade: "); //Promts user for input.
+        String userGrade = input.nextLine().toUpperCase();//Reads the user input and returns a string in upper case.
 
         switch (userGrade) {
             case "A+":
             case "A":
-                System.out.println("Distinction");
+                System.out.println("Distinction"); //If grade is A or A+ the output is "Distinction".
                 break;
             case "B":
-                System.out.println("B Grade");
+                System.out.println("B Grade"); //If the grade is not A or A+ and not C or Fail and B is entered then "B Grade" is output.
                 break;
             case "C":
-                System.out.println("C Grade");
+                System.out.println("C Grade"); //If the grade is not A, A+, B or fail and C is entered then the output is "C Grade".
                 break;
             default:
-                System.out.println("Fail");
+                System.out.println("Fail"); //If all other cases are false and or any other input is put in then the output is "Fail".
                 break;
         }
 
