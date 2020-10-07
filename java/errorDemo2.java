0b6eda8f46cb7994c169b49c86109843989d17c7
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-10-06 23:59:32 -0500

    doing a second error handling demo

---
+++ java/errorDemo2.java
@@ -0,0 +1,16 @@
+package errordemo2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ErrorDemo2{
+
+    public static void main(String[] args){
+        int choice = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        int[] numbers = { 10, 11, 12, 13, 14, 15 };
+        System.out.print("Please enter the index of the array: ");
+    }
+}
