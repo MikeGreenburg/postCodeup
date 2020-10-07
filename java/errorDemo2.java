91a696e150e5918cd18961f54a0e7493c060ef9c
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-10-07 00:04:07 -0500

    finishing up the error demo 2

--- java/errorDemo2.java
+++ java/errorDemo2.java
@@ -12,5 +12,19 @@ public class ErrorDemo2{
 
         int[] numbers = { 10, 11, 12, 13, 14, 15 };
         System.out.print("Please enter the index of the array: ");
+
+        try{
+            choice = input.nextInt();
+            System.out.printf("numbers[%d] = %d%n", choice, numbers[choice]);
+        }
+        catch (ArrayIndexOutOfBoundsExecption e){
+            System.out.println("Error: Index is invalid.");
+        }
+        catch (InputMismatchExecption e){
+            System.out.println("Error: You did not enter an integer.");
+        }
+        catch (Exception e){
+            System.out.printf(e.getMessage());
+        }
     }
 }
