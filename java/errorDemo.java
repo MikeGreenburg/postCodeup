1eafc7e7bc3ac4079202fcb6ad468df83b3e50fa
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-10-06 23:54:59 -0500

    added some comments on my Error Demo

--- java/errorDemo.java
+++ java/errorDemo.java
@@ -25,4 +25,5 @@ public class ErrorDemo{
             System.out.println("---- End of Error Handling Example ----");
         }
     }
-}
+}//If you run the code and enter 12 and 4 you will get the message "The result is 3 ---- End of Error Handiling Example ----"
+//If you run the program again and enter 12 and 0 you will get "/ by zero ---- End of Error Handling Example ----"
